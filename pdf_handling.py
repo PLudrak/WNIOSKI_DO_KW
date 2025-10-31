28033e92c354414814471d21bc28455d4e7276f8
PLudrak <jakwojcicki97@gmail.com>
2025-10-31 12:03:11 +0100

    łączenie wszystkich wniosków i pierwszych stron

--- pdf_handling.py
+++ pdf_handling.py
@@ -5,14 +5,20 @@ from pypdf import PdfReader, PdfWriter, PdfMerger
 class PDFRegistry:
     _stats: list[dict] = []
     _dirs = []
+    _wnioski = []
 
     @classmethod
-    def add(cls, path):
-        stat = {"path": path, "pages": cls.pages_counter(path)}
-        cls._stats.append(stat)
+    def add(cls, path, wniosek=False):
+        if not wniosek:
+            stat = {"path": path, "pages": cls.pages_counter(path)}
+            cls._stats.append(stat)
+        else:
+            cls._wnioski.append(path)
 
     @classmethod
-    def get_all(cls):
+    def get_all(cls, wniosek=False):
+        if wniosek:
+            return cls._wnioski.copy()
         return cls._stats.copy()
 
     @classmethod
@@ -35,9 +41,23 @@ class PDFRegistry:
         return set(dirs_list)
 
 
+def merge_all():
+    if (
+        input(
+            "Czy łączyć wnioski? (T/N)\nPrzed połączeniem możesz wprowadzić zmiany w plikach pdf"
+        ).upper()
+        == "T"
+    ):
+        merge_wniosek()
+        merge_wnioski_obreb()
+        merge_first_pages_wnioski()
+    else:
+        print("Wnioski i załączniki znajdują się w poszczególnych podfolderach")
+
+
 def dodaj_puste_strony():
     """dodaje pustą stronę na końcu wniosku lub załącznika jeżeli liczba stron jest nieparzysta"""
-    print("merging")
+    print("Poprawiam parzystość stron dla druku dwustronnego")
     lista_pdf = PDFRegistry.get_all()
 
     for item in lista_pdf:
@@ -63,6 +83,7 @@ def dodaj_puste_strony():
 
 def merge_wniosek():
     """Połącz poszczególne strony i załączniki we wnioski na podstawie ścieżek z PDFRegistry"""
+    print("Łączę załączniki we wnioski")
     dodaj_puste_strony()
     grouped_files = {}
 
@@ -84,6 +105,7 @@ def merge_wniosek():
 
 
 def merge_wnioski_obreb(base_dir="export"):
+    print("Łączę wnioski wg obrębów")
     for sad in os.listdir(base_dir):
         sad_path = os.path.join(base_dir, sad)
         if not os.path.isdir(sad_path):
@@ -97,17 +119,43 @@ def merge_wnioski_obreb(base_dir="export"):
             writer = PdfWriter()
             for nr_kw in os.listdir(obreb_path):
                 nr_kw_path = os.path.join(obreb_path, nr_kw)
-                wniosek_path = os.path.join(nr_kw_path, "Wniosek.pdf")
+                if not os.path.isdir(nr_kw_path):
+                    continue
 
-                if os.path.isfile(wniosek_path):
-                    reader = PdfReader(wniosek_path)
-                    for page in reader.pages:
-                        writer.add_page(page)
+                # Szukaj plików PDF z "wniosek" w nazwie
+                for fname in os.listdir(nr_kw_path):
+                    if fname.lower().endswith(".pdf") and "wniosek" in fname.lower():
+                        wniosek_path = os.path.join(nr_kw_path, fname)
+                        PDFRegistry.add(wniosek_path, wniosek=True)
+                        reader = PdfReader(wniosek_path)
+                        for page in reader.pages:
+                            writer.add_page(page)
 
             output_pdf_path = os.path.join(sad_path, f"{obreb}_wnioski.pdf")
             if writer.pages:
                 with open(output_pdf_path, "wb") as f:
                     writer.write(f)
-                print(f"Zapisano: {output_pdf_path}")
+                print(f" ->Zapisano: {output_pdf_path}")
             else:
                 print(f"Brak wniosków do połączenia w: {obreb_path}")
+
+
+def merge_first_pages_wnioski(output_name="pierwsze_strony_wnioski.pdf"):
+    """
+    Tworzy nowy plik PDF zawierający pierwsze strony wszystkich plików
+    z PDFRegistry._wnioski.
+    """
+    output_path = os.path.join("export", output_name)
+    writer = PdfWriter()
+
+    for pdf_path in PDFRegistry.get_all(wniosek=True):
+        reader = PdfReader(pdf_path)  # type: ignore
+        if len(reader.pages) > 0:
+            writer.add_page(reader.pages[0])  # dodajemy tylko pierwszą stronę
+
+    if writer.pages:
+        with open(output_path, "wb") as f:
+            writer.write(f)
+        print(f"Zapisano plik z pierwszymi stronami: {output_path}")
+    else:
+        print("Brak stron do zapisania.")
