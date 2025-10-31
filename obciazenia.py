92fcab11f2666e9c87796b0d51b273fd7f5aaf0c
PLudrak <jakwojcicki97@gmail.com>
2025-10-31 12:45:25 +0100

    poprawione generowanie treści obciazenia

--- obciazenia.py
+++ obciazenia.py
@@ -1,3 +1,4 @@
+import re
 import pandas as pd
 from data_loading import load_obciazenia
 from wniosek import krotkie_id
@@ -7,20 +8,42 @@ def get_obciazone_kw(df_obciazenia):
     return df_obciazenia["kw"].dropna().unique()
 
 
-def stworz_tresc_obicazenia(id_dzialki, kolor: str, tresc_obciazenia, kw, obreb):
+def usun_spacje_przed_znakami(text):
+    # Usuwa spacje przed przecinkami i kropkami
+    text = re.sub(r"\s+([,.])", r"\1", text)
+    # Opcjonalnie – usuń podwójne spacje
+    text = re.sub(r"\s{2,}", " ", text)
+    return text.strip()
 
+
+def popraw_przypadek_obciazenia(tresc_obciazenia: str) -> str:
+    zmiany = {"PRZEBUDOWA": "PRZEBUDOWĄ", "BUDOWA": "BUDOWĄ", "ROZBIÓRKA": "ROZBIÓRKĄ"}
+    for slowo, forma in zmiany.items():
+        tresc_obciazenia = tresc_obciazenia.replace(slowo, forma)
+    return tresc_obciazenia
+
+
+def popraw_tresc_obciazenia(tresc_obciazenia: str) -> str:
+    tresc_obciazenia = popraw_przypadek_obciazenia(tresc_obciazenia.upper())
+    tresc_obciazenia = str(tresc_obciazenia).replace(" \n", ", ")
+    tresc_obciazenia = usun_spacje_przed_znakami(tresc_obciazenia)
+
+    return tresc_obciazenia
+
+
+def stworz_tresc_obicazenia(id_dzialki, kolor: str, tresc_obciazenia, kw, obreb):
+    tresc_obciazenia = popraw_tresc_obciazenia(tresc_obciazenia)
     id_dzialki = krotkie_id(str(id_dzialki))
     koloru = str(kolor).replace("ki", "kiego").replace("wy", "wego").upper()
-    tresc_obciazenia = str(tresc_obciazenia).replace(" \n", ", ")
     tresc = f"""
     WNOSZĘ O UJAWNIENIE W DZIALE III KW {kw} OGRANICZENIA SPOSOBU KORZYSTANIA Z NIERUCHOMOŚCI, STANOWIĄCEJ DZIAŁKĘ EW. 
     NR. {id_dzialki} OBR. {obreb} W OBSZARZE OZNACZONYM PRZERYWANĄ LINIĄ KOLORU {koloru} W PROJEKCIE ZAGOSPODAROWANIA TERENU, 
     STANOWIĄCYM CZĘŚĆ ZATWIERDZONEGO DECYZJĄ, PROJEKTU BUDOWLANEGO, POPRZEZ UDZIELENIE GENERALNEMU DYREKTOROWI DRÓG KRAJOWYCH I 
-    AUTOSTRAD ZEZWOLENIA NA PROWADZENIE NA WSKAZANEJ WSKAZANEJ CZESCI NIERUCHOMOSCI PRAC ZWIĄZANYCH Z {tresc_obciazenia.upper()}.
+    AUTOSTRAD ZEZWOLENIA NA PROWADZENIE NA WSKAZANEJ CZESCI NIERUCHOMOSCI PRAC ZWIĄZANYCH Z {tresc_obciazenia}.
     WNOSZĘ O WPISANIE, ŻE WŁAŚCICIEL NIERUCHOMOŚCI JEST ZOBOWIĄZANY DO UDOSTĘPNIENIA OPISANEJ WYŻEJ NIERUCHOMOŚCI NA RZECZ KAŻDOCZESNEGO
     WŁAŚCICIELA SIECI W CELU WYKONANIA CZYNNOŚCI ZWIĄZANYCH Z KONSERWACJĄ ORAZ USUWANIEM AWARII CIĄGÓW, PRZEWODÓW I URZĄDZEŃ NA PODSTAWIE
     OSTATECZNEJ DECYJI WOJEWODY PODLASKIEGO NR. 11/2023 O ZEZWOLENIU NA REALIZACJĘ INWESTYCJI DROGOWEJ Z DNIA 27 WRZEŚNIA 2024. 
-    DO OGRANICZEŃ O KTÓRYCH MOWA POWYŻEJ STOSUJE SIĘ ODPOWIEDNIO PRZEPISY ART.P 124 UST. 4-8 I ART. 124A USTAWY Z DNIA 21 SIERPNIA 1997 R.
+    DO OGRANICZEŃ O KTÓRYCH MOWA POWYŻEJ STOSUJE SIĘ ODPOWIEDNIO PRZEPISY ART.124 UST. 4-8 I ART. 124A USTAWY Z DNIA 21 SIERPNIA 1997 R.
     O GOSPODARCE NIERUCHOMOŚCIAMI.
     """
 
@@ -86,7 +109,7 @@ def get_obciazenia_bez_odlaczen(lista_kw, obciazenia, df_dzialki):
 
         if kw not in lista_nr_kw:
             # wyszukiwanie info o obrebie i jr na podstawie pierwszej dzialki w KW
-            print(kw)
+            # print(kw)
             rekord = df_dzialki[df_dzialki["KW"] == kw]
 
             if not rekord.empty:
